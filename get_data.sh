e82d0fbcc411357a78ee64c167aaf13c3e4e4169
hadyelsahar <hadyelsahar@gmail.com>
2016-12-26 13:15:30 +0100

    install json files instead of TFRecord

--- get_data.sh
+++ get_data.sh
@@ -7,11 +7,11 @@ wget https://github.com/dmorr-google/wiki-reading/blob/master/data/answer.vocab
 wget https://github.com/dmorr-google/wiki-reading/blob/master/data/document.vocab
 wget https://github.com/dmorr-google/wiki-reading/blob/master/data/raw_answer.vocab
 wget https://github.com/dmorr-google/wiki-reading/blob/master/data/type.vocab
-wget -c https://storage.googleapis.com/wikireading/validation.tar.gz
-tar xvzf validation.tar.gz &
-wget -c https://storage.googleapis.com/wikireading/test.tar.gz
-tar xvzf test.tar.gz &
-wget -c https://storage.googleapis.com/wikireading/train.tar.gz
-tar xvzf train.tar.gz
+wget -c https://storage.googleapis.com/wikireading/validation.json.tar.gz
+tar xvzf validation.json.tar.gz &
+wget -c https://storage.googleapis.com/wikireading/test.json.tar.gz
+tar xvzf test.json.tar.gz &
+wget -c https://storage.googleapis.com/wikireading/train.json.tar.gz
+tar xvzf train.json.tar.gz
 
 echo "Done."
